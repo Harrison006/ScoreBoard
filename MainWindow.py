2da965bf22c49ab313164236cd4b42ccecb59dd9
Harrison006 <87344112+Harrison006@users.noreply.github.com>
2022-04-28 12:19:38 +1000

    Create MainWindow.py

---
+++ MainWindow.py
@@ -0,0 +1,6 @@
+import imp
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtCore import QCoreApplication
+from PyQt6.QtGui import QPixmap
+from Ui_scoreboard import Ui_MainWindow
