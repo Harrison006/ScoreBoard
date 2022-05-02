980965c87b64ab5bc4c908853ddff3d55a1d6d03
Harrison006 <Harrison006@outlook.com.au>
2022-05-02 21:26:15 +1000

    Update MainWindow.py

--- MainWindow.py
+++ MainWindow.py
@@ -1,6 +1,26 @@
 import imp
 import sys
+from Ui_scoreboard import Ui_MainWindow
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from PyQt6.QtCore import QCoreApplication
 from PyQt6.QtGui import QPixmap
-from Ui_scoreboard import Ui_MainWindow
+class MainWindow:
+    def __init__(self):
+        #ui elements
+        self.main_win = QMainWindow()
+        self.ui =Ui_MainWindow
+        self.ui.setupUi(self.main_win)
+        #init ui with values
+        self.leftscore()
+        self.rightscore()
+        self.timer()
+        self.leftteam()
+        self.rightteam()
+        self.settimer()
+
+
+    def show(self):
+        self.main_win.show()
+
+    def leftscore(self):
+        self.ui
