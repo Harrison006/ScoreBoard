009f6a4982b12e362fc1cfef9f4df552b274642d
Harrison006 <87344112+Harrison006@users.noreply.github.com>
2022-05-05 14:06:16 +1000

    .

--- main_window.py
+++ main_window.py
@@ -8,8 +8,15 @@ class MainWindow:
     def __init__(self):
         #ui elements
         self.main_win = QMainWindow()
-        self.ui =Ui_MainWindow
+        self.ui = Ui_MainWindow
         self.ui.setupUi(self.main_win)
+        #init game var
+        self.timer = 0
+        self.leftscore = 0
+        self.rightscore = 0
+        self.leftteam = ""
+        self.rightteam = ""
+        self.settimer = 0
         #init ui with values
         self.leftscore()
         self.rightscore()
@@ -23,4 +30,11 @@ class MainWindow:
         self.main_win.show()
 
     def leftscore(self):
-        self.
+        if self.ui.points_2_left.clicked.connect(leftscore = +1):
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_win = MainWindow()
+    main_win.show()
+    sys.exit(app.exec())
